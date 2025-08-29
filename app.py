66f37235709aa3e274e8fb4d3972a57a2b802fa9
Naik Pragni <129032980+Pragni24@users.noreply.github.com>
2025-08-29 11:13:55 +0530

    update app.py

--- app.py
+++ app.py
@@ -55,5 +55,8 @@ def bfhl():
             "error": str(e)
         }), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or default 5000
+    app.run(host='0.0.0.0', port=port)
